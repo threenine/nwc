91e817d9f4302006c5e7ca13c8a90e9053360a51
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-09-29 18:48:22 +0100

    sample

--- components/TwcHeader.vue
+++ components/TwcHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <Popover class="relative bg-yellow-100 mb-1">
+  <Popover class="relative bg-yellow-50 mb-1">
     <div class="absolute inset-0 shadow z-30 pointer-events-none" aria-hidden="true" />
     <div class="relative z-20">
       <div class="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
