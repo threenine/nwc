8071471e992be266d3be0af325b3b115916d2e73
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-09-30 09:47:16 +0100

    Adjusting alt text

--- components/TwcHero.vue
+++ components/TwcHero.vue
@@ -18,7 +18,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
 
-              <span class="mx-2">{{ item.benefit }}</span>
+              <span class="mx-2 font-medium">{{ item.benefit }}</span>
             </p>
 
           </div>
