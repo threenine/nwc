a2620a1e0a90dbdf01c2c81f6dca661417fae035
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-10-13 10:43:02 +0100

    Updated hero image

--- components/TwcHero.vue
+++ components/TwcHero.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative">
     <div class="absolute inset-0">
-      <img class="h-full w-full object-cover" src="~/assets/img/flower-sunset.jpg" alt="natashawoodfine.com privacy policy" />
-      <div class="absolute inset-0 bg-gray-400 mix-blend-multiply" aria-hidden="true" />
+      <img class="h-full w-full object-cover" src="~/assets/img/flower-sunset.jpg" alt="natashawoodfine.com" />
+      <div class="absolute inset-0 bg-gray-100 mix-blend-multiply" aria-hidden="true" />
     </div>
     <div class="relative container flex flex-col px-6 py-10 mx-auto space-y-6 lg:h-[32rem] lg:py-16 lg:flex-row lg:items-center">
 
