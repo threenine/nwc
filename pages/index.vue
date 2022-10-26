ebfe90e91c9daacb6a2bd8a17f917ad9f00ba2b4
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-10-26 09:01:44 +0100

    Updated

--- pages/index.vue
+++ pages/index.vue
@@ -8,26 +8,27 @@
           <span class="text-3xl font-bold">Welcome</span>
           </div>
           <div class="mx-auto max-w-prose text-lg">
-            <p class="mt-8 text-xl leading-8 text-gray-500">I am a fully qualified counsellor working in the Swindon area. I offer counselling to adults and young people from age 16 upwards, covering a wide range  of issues.</p>
+            <p class="mt-8 text-xl leading-8 text-gray-500">I am a fully qualified counsellor working in the Swindon area,
+              offering counselling to adults and young people from age 16 upwards, covering a wide range  of issues.</p>
           </div>
           <div class="prose prose-lg prose-indigo mx-auto mt-6 text-gray-500">
             <span class="text-2xl font-bold">About counselling</span>
             <p>Counselling can support you to help yourself in a safe and confidential environment, offering you an opportunity to feel less isolated in your situation. To talk and be truly heard by someone who is outside of your usual social circle, to share your thoughts and feelings which you cannot share elsewhere in your life.</p>
-            <p>Having the confidential space to talk can help some people find a way forward. Counselling isn’t about giving advice, it is about being aware of the choices you have and which ones feel right for you to make. </p>
+            <p>Having the confidential space to talk can help some people find a way forward. Counselling isn’t about giving advice, it is about being aware of the choices you have and which ones feel right for you to make.  </p>
 
             <span class="text-2xl font-bold">Why counselling</span>
-            <p>Sometimes it is easy to feel that you are expected to be able to cope with everything that is going on….</p>
+            <p>Sometimes it is easy to feel that you are expected to be able to cope with everything that is going on.</p>
             <ul role="list">
               <li>You might be struggling to understand your own feelings and what is going on for you at this moment. </li>
               <li>You may be experiencing distress, anxiety and emotion about a situation or trying to manage a crisis or life changing event. </li>
-              <li>It may be that you feel lost, a low self esteem or depressed and are struggling to understand how or why you feel this way. </li>
+              <li>It may be that you feel lost, have low self esteem or depressed and are struggling to understand how or why you feel this way.  </li>
               <li>You might feel emotional and uncertain about a situation, person or relationship.</li>
-              <li>You may think that your emotional well-being is not what it ought to be or have something on your mind………</li>
+              <li>You may think that your emotional well-being is not what it ought to be or have something on your mind.</li>
             </ul>
-            <p>Talking about these situations can feel daunting, however reaching out to talk could be what it takes to stop suppressing these concerns, which can help your well-being and not lead to depression or anxiety.</p>
+            <p>Talking about these situations can feel daunting, however reaching out to talk could be what it takes to stop suppressing these concerns, which can help your well-being.</p>
             <span class="text-2xl font-bold">How can we work together</span>
-            <p>My approach to counselling is a person-centred one. I feel the relationship between client and counsellor is fundamental in helping to provide the supportive environment where you can have the opportunity to gain a better understanding of yourself and the challenges you are facing. Here you will be able to explore what is concerning you, without judgement, keeping at your pace where I will truly hear you as you present your thoughts and feelings. I am led by you the client, and believe that given the right environment you hold the answers to your own freedom and growth. I will listen and explore what is going on for you and reflect back the impact this has had on you. </p>
-            <p>Your journey to seek counselling is as unique as you are which is my fundamental belief within the person centred modality that I work. </p>
+            <p>My approach to counselling is a person-centred one. I feel the relationship between client and counsellor is fundamental in helping to provide the supportive environment where you can have the opportunity to gain a better understanding of yourself and the challenges you are facing. Here you will be able to explore what is concerning you, without judgement, keeping at your pace where I will truly hear you as you present your thoughts and feelings. I am led by you the client, and believe that given the right environment you hold the answers to your own freedom and growth. I will listen and we can explore what is going on for you and reflect back the impact this has had on you.  </p>
+            <p>Your journey to seek counselling is as unique as you are which is my fundamental belief within the person-centred modality that I work. </p>
           </div>
         </div>
 
