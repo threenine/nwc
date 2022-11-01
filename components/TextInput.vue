f6f27b03219d3f20a92879d47d9deeafbe11b194
Gary Woodfine <gary.woodfine@threenine.co.uk>
2022-11-01 20:05:33 +0000

    updated styles for text input boxes

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -3,7 +3,7 @@
       <Field :name="name" v-slot="{ field, meta, errors }">
         <input v-bind="field"
                class="block w-full rounded-md border-gray-300 shadow-sm focus:border-grape-500 focus:ring-grape-500 sm:text-sm"
-               :class="{'text-green-600 dark:text-green-500' : meta.valid && meta.touched ,
+               :class="{'border-green-600 dark:border-green-500' : meta.valid && meta.touched ,
                         'text-red-600 dark:text-red-500' : !meta.valid && meta.touched
                }"
                :placeholder="placeholder"
