f0b72df810a7dcc7ca9d3eb15b262d1f5178d9de
Gary Woodfine <garywoodfine@duck.com>
2023-08-23 09:37:19 +0100

    added missing files

---
+++ plugins/fathom.client.ts
@@ -0,0 +1,16 @@
+import { defineNuxtPlugin } from '#app'
+// eslint-disable-next-line import/no-named-as-default
+import VueFathom from '@ubclaunchpad/vue-fathom'
+// noinspection JSUnusedGlobalSymbols
+export default defineNuxtPlugin((nuxtApp) => {
+    const config = useRuntimeConfig()
+    nuxtApp.vueApp.use(VueFathom, {
+        siteID: config.public.fathom_analytics_id,
+        settings: {
+            url: 'https://cdn.usefathom.com/script.js',
+            spa: 'history',
+            excludedDomains: ['localhost'],
+        },
+    })
+})
+
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [  "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@headlessui/tailwindcss'),
+  ],
+} satisfies Config
+
+
+
